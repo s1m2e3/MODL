8a7fe5492742c4647db7e87daaf313c4238b8ba7
s1m2e3 <samuelcornejo@arizona.edu>
2025-04-14 23:03:47 -0500

    feat: added first constraints

--- car_following/system_model.py
+++ car_following/system_model.py
@@ -24,16 +24,38 @@ def binary_angle_change_negative(x,x_target):
     
     return -(angle_target-angle_ego)
 
-def angle_change_positive(x):
-    _, _, _, _, steering_angle_ego = x
+def angle_change_positive(x,u,binary_angle_change_positive,l=1.0,big_m=1.0):
+    _, _, velocity, _, steering_angle = x
+    acceleration, _ = u
     
-    return -steering_angle_ego
+    angle_change_positive = (torch.atan((big_m*(1-binary_angle_change_positive)*l)/(velocity+acceleration))-steering_angle)
 
-def angle_change_negative(x):
-    _, _, _, _, _, steering_angle_ego = x
+    return angle_change_positive
+
+def angle_change_negative(x,u,binary_angle_change_negative,l=1.0,big_m=1.0):
+    _, _, velocity, _, steering_angle = x
+    acceleration, _ = u
     
-    return -steering_angle_ego
+    angle_change_negative = (torch.atan(-(big_m*(1-binary_angle_change_negative)*l)/(velocity+acceleration))-steering_angle)
 
+    return angle_change_negative
+
+def steering_angle_rate_upper_bound(u,steering_angle_rate_bound):
+    _, steering_angle_rate = u
+    return -steering_angle_rate_bound
+
+
+def steering_angle_rate_lower_bound(u,steering_angle_rate_bound):
+    _, steering_angle_rate = u
+    return steering_angle_rate_bound
+
+def acceleration_upper_bound(u,acceleration_bound):
+    acceleration, _ = u
+    return acceleration_bound
+
+def acceleration_lower_bound(u,acceleration_bound):
+    acceleration,_ = u
+    return -acceleration_bound
 
 def system_dynamics(x, u, dt=0.01,l=1.0):
     """ x = [position_x, position_y,velocity, angle,stering_angle], u = [acceleration,steering_angle_rate] """
@@ -57,8 +79,8 @@ def get_random_target_state(x,max_change_x,min_change_x,max_change_y,min_change_
     sign_x = 1 if torch.rand(1).item() < 0.5 else -1
     sign_y = 1 if torch.rand(1).item() < 0.5 else -1
     sign_angle = 1 if torch.rand(1).item() < 0.5 else -1
-    straight_lane = torch.rand(1).item() < 0.5
-
+    # curve = torch.rand(1).item() < 0.01
+    curve = True
     delta_x_min = min_change_x * sign_x
     delta_x_max = max_change_x*torch.rand(1) * sign_x
     delta_y_min = min_change_y * sign_y
@@ -68,7 +90,7 @@ def get_random_target_state(x,max_change_x,min_change_x,max_change_y,min_change_
 
     new_position_x = position_x + min(delta_x_min,delta_x_max)
     new_position_y = position_y + min(delta_y_min,delta_y_max)
-    new_angle = angle + min(delta_angle_min,delta_angle_max) if straight_lane else angle
+    new_angle = angle + min(delta_angle_min,delta_angle_max) if curve else angle
     return torch.stack([new_position_x,new_position_y,torch.tensor([0]),new_angle,torch.tensor([0])])
     
 def get_inertial_target_state(x,target):
